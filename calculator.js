c45459b6695d863b1c0d75202aaed3a07691a563
hafiz salman <hmsalmans1@gmail.com>
2020-03-04 13:42:49 -0500

    added arithmetic ability functions

--- calculator.js
+++ calculator.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
 
 
     // Variables needed
-    
+    var pii = 3.14;
     var number1 = "";
     var number2 = "";
     var operator = "";
     var firstNumber = true;
+    var result;
     
     
     
@@ -17,10 +18,11 @@ $(document).ready(function() {
             number1 += $(this).attr("value");
             $("#first").html(number1);
         }
-        else {
+        else  {
             number2 += $(this).attr("value");
             $("#second").html(number2);
         }
+
     
     });
     
@@ -30,17 +32,70 @@ $(document).ready(function() {
     
     
     $(".operator").on("click", function(){
-        firstNumber = flase;
-        if (firstNumber){
-            number1 += $(this).attr("value");
-            $("#first").html(number1);
-        }
-        else {
-            number2 += $(this).attr("value");
-            $("#second").html(number2);
-        }
+        firstNumber = false;
+          if ( operator = $(this).attr("value"));
+            $("#operator").html($(this).text());
+        
+        });
+
+
+   
+        
+
+     
+    $(".equal").on("click", function (){
+
+        number1 = parseInt(number1);
+        number2 = parseInt(number2);
+
+       
+
+       if (operator == "plus"  ) {
+           
+           result = number1 + number2;
+
+    } 
+
+       else if (operator == "minus"  ) {
+        result = number1 - number2;
+
+    }
+
+        else if (operator == "times"  ) {
+        result = number1 * number2;
+
+    }   
+
+        else if (operator == "divide"  ) {
+        result = number1 / number2;
+
+    }  
+
     
-    });
+     $("#result").html(result.toFixed(2));
+
+     ready();
+
+     
+
+
+        });
+
+
+        $(".clear").on("click", function()
+        {
+            $(".data").remove();
+
+            location.reload();
+
+            
+
+        })
+        
+
+
+
+
     
     
     
