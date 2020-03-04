88529540ea2d64899fb3ae048495f69a2cfcf051
hafiz salman <hmsalmans1@gmail.com>
2020-03-04 10:00:44 -0500

    created functions for number value

--- calculator.js
+++ calculator.js
@@ -1,36 +1,73 @@
 $(document).ready(function() {
 
 
-// Variables needed
-
-var number1 = "";
-var number2 = "";
-var operator = "";
-var firstNumber = true;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-)};
+    // Variables needed
+    
+    var number1 = "";
+    var number2 = "";
+    var operator = "";
+    var firstNumber = true;
+    
+    
+    
+    //Building functions for number keys
+    
+    $(".number").on("click", function(){
+        if (firstNumber){
+            number1 += $(this).attr("value");
+            $("#first").html(number1);
+        }
+        else {
+            number2 += $(this).attr("value");
+            $("#second").html(number2);
+        }
+    
+    });
+    
+    
+    
+    //Building functions for operations
+    
+    
+    $(".operator").on("click", function(){
+        firstNumber = flase;
+        if (firstNumber){
+            number1 += $(this).attr("value");
+            $("#first").html(number1);
+        }
+        else {
+            number2 += $(this).attr("value");
+            $("#second").html(number2);
+        }
+    
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    });
